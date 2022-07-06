2b180b0ed24586571f0118be5b55d0e695e33ad3
MIYABETaisei <mytspe46@gmail.com>
2022-07-07 07:27:28 +0900

    ✨

---
+++ src/component/Square.tsx
@@ -0,0 +1,27 @@
+type Player = "X" | "O" | "BOTH" | null;
+function Square({
+  value,
+  onClick,
+  winner,
+}: {
+  winner: Player;
+  value: Player;
+  onClick: () => void;
+}) {
+  if (!value) {
+    return (
+      <button
+        className="square"
+        onClick={onClick}
+        disabled={Boolean(winner)}
+      ></button>
+    );
+  }
+  return (
+    <button className={`square square_${value.toLocaleLowerCase()}`} disabled>
+      {value}
+    </button>
+  );
+}
+
+export default Square;
